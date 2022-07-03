e63a9107640e2f5fd682b81c297f563dca838ea8
Juan Camilo Soto Pineda <lanea07@outlook.com>
2022-07-03 15:58:23 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://lanea07:Marianita.07@cluster0.dwa4ql3.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB;
